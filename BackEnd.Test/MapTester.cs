026c0aca6a3a153d2c874eeaebfef4ccf9521154
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-03-08 12:10:40 +0100

    Map : GenerateMap

--- BackEnd.Test/MapTester.cs
+++ BackEnd.Test/MapTester.cs
@@ -5,9 +5,23 @@ namespace BackEnd.Test
     [TestClass]
     public class MapTester
     {
+        Map map = new Map() { 
+            Size = 8
+        };
+
         [TestMethod]
-        public void TestMethod1()
+        public void Map_GenerateMap_WithSize8_ThenListCount64()
         {
+            var listTile = map.GenerateMap();
+            Assert.AreEqual(listTile.Count, 64);
+        }
+
+        [TestMethod]
+        public void Map_GenerateMap_WithSize0_ThenListCount0()
+        {
+            map.Size = 0;
+            var listTile = map.GenerateMap();
+            Assert.AreEqual(listTile.Count, 0);
         }
     }
 }
--- BackEnd/Map.cs
+++ BackEnd/Map.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace BackEnd
 {
-    internal class Map
+    public class Map
     {
+        public int Size { get; set; }
+        public List<Tile> Tiles { get; set; }
+
+        public List<Tile> GenerateMap()
+        {
+            List<Tile> tilesGenerated = new List<Tile>();
+            Tile t = null;
+            for(int x = 0; x < Size; x++)
+            {
+                for (int y = 0; y < Size; y++)
+                {
+                    t = new Tile();
+                    tilesGenerated.Add(t);
+                }
+            }
+            return tilesGenerated;
+        }
     }
 }

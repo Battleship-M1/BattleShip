a1b766976d107a101c671e1113c2a920530b3b64
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-03-24 12:31:31 +0100

    Boat

--- BackEnd.Test/BoatTester.cs
+++ BackEnd.Test/BoatTester.cs
@@ -5,9 +5,62 @@ namespace BackEnd.Test
     [TestClass]
     public class BoatTester
     {
-        [TestMethod]
-        public void TestMethod1()
+        Boat b = new Boat();
+        Map blankMap = new Map(0);
+        Map okMap = new Map(8);
+        int okLength = 2;
+        int failLength = -1;
+        Tile okTile = new Tile()
         {
+            X = 1,
+            Y = 1
+        };
+        Tile failTile = new Tile()
+        {
+            Y = -5,
+            X = 0,
+        };
+
+        [TestMethod]
+        public void Boat_IsOnMap_WithBlankGrid_ThenFalse()
+        {
+            Assert.IsFalse(b.IsOnMap(blankMap, okTile, okLength, Enum.Alignement.HORIZONTAL));
+        }
+
+        [TestMethod]
+        public void Boat_IsOnMap_WithNOKTopLeft_ThenFalse()
+        {
+            Assert.IsFalse(b.IsOnMap(okMap, failTile, okLength, Enum.Alignement.HORIZONTAL));
+        }
+
+        [TestMethod]
+        public void Boat_IsOnMap_WithNegativeLength_ThenFalse()
+        {
+            Assert.IsFalse(b.IsOnMap(okMap, okTile, failLength, Enum.Alignement.HORIZONTAL));
+        }
+
+        [TestMethod]
+        public void Boat_IsOnMap_WithPositiveLengthAndOKTopLeft_ThenTrue()
+        {
+            Assert.IsTrue(b.IsOnMap(okMap, okTile, okLength, Enum.Alignement.HORIZONTAL));
+        }
+
+        [TestMethod]
+        public void Boat_GenerateTilesUsed_WithNegativeOrZeroLength_ThenNull()
+        {
+            Assert.IsNull(b.GenerateTilesUsed(okTile, -1, Enum.Alignement.HORIZONTAL));
+        }
+
+        [TestMethod]
+        public void Boat_GenerateTilesUsed_WithNOKTopLeft_ThenNull()
+        {
+            Assert.IsNull(b.GenerateTilesUsed(failTile, 2, Enum.Alignement.HORIZONTAL));
+        }
+
+        [TestMethod]
+        public void Boat_GenerateTilesUsed_WithOKTopLeft3Length_ThenListCount3()
+        {
+            Assert.AreEqual(3, b.GenerateTilesUsed(okTile, 3, Enum.Alignement.HORIZONTAL));
         }
     }
 }
--- BackEnd/Boat.cs
+++ BackEnd/Boat.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using BackEnd.Enum;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BackEnd
 {
-    internal class Boat
+    public class Boat
     {
+        public int Length { get; set; }
+        public Alignement Alignement { get; set; }
+        public List<Tile> TilesUsed { get; set; }
+
+        public bool IsOnMap(Map m, Tile topLeft, int length, Alignement alignement)
+        {
+            if (m.Size < 1) { return false; }
+            if(length < 1) { return false; }
+            if(!(topLeft.IsOnMap(m))){ return false; }
+            if(alignement == Alignement.VERTICAL)
+            {
+                topLeft.Y += length;
+            }
+            else
+            {
+                topLeft.X += length;
+            }
+            if(!(topLeft.IsOnMap(m))){ return false; }
+            return true;
+        }
+
+        public List<Tile> GenerateTilesUsed(Tile topLeft, int length, Alignement alignement)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

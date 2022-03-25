43bb8802d415691395681ccf2177a171c7bebf8e
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-03-25 15:42:18 +0100

    Modif front : map + bateaux initiaux + commencer la partie

---
+++ WebApplication1/Pages/Game.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BackEnd;
+
+namespace WebApplication.Pages.Shared
+{
+    public class Game : PageModel
+    { 
+        public void OnGet()
+        {
+        }
+    }
+}

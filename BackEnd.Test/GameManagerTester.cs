ce4916106b8652423a6c7eca806fb6417ea347f3
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-01 22:15:34 +0200

    GameManager

--- BackEnd.Test/GameManagerTester.cs
+++ BackEnd.Test/GameManagerTester.cs
@@ -7,7 +7,9 @@ namespace BackEnd.Test
     public class GameManagerTester
     {
         GameManager gm = new GameManager();
+        Map testMap8 = new Map(8);
 
+        #region GenerateDefaultBoats
         [TestMethod]
         public void GenerateDefaultBoats_LengthIs6()
         {
@@ -57,5 +59,94 @@ namespace BackEnd.Test
             }
             Assert.AreEqual(1, length5amount);
         }
+        #endregion GenerateDefaultBoats
+
+        #region MoveBoat
+        [TestMethod]
+        public void MoveBoat_TopLeftOutsideMap_ThenFalse()
+        {
+            Boat b = new Boat(2, new Tile(1, 0));
+            Assert.IsFalse(gm.MoveBoat(ref b, testMap8, Enum.Direction.TOP));
+        }
+
+        [TestMethod]
+        public void MoveBoat_TopLeftInsideMap_ThenTrue()
+        {
+            Boat b = new Boat(2, new Tile(1, 1));
+            Assert.IsTrue(gm.MoveBoat(ref b, testMap8, Enum.Direction.TOP));
+        }
+
+        [TestMethod]
+        public void MoveBoat_DirectionTOP_Ydecrease_CheckResult()
+        {
+            Boat b = new Boat(2, new Tile(1, 1));
+            gm.MoveBoat(ref b, testMap8, Enum.Direction.TOP);
+            Assert.AreEqual(0, b.topLeft.Y);
+            Assert.AreEqual(1, b.topLeft.X);
+        }
+
+        [TestMethod]
+        public void MoveBoat_DirectionBOT_Yincrease_CheckResult()
+        {
+            Boat b = new Boat(2, new Tile(1, 1));
+            gm.MoveBoat(ref b, testMap8, Enum.Direction.BOTTOM);
+            Assert.AreEqual(2, b.topLeft.Y);
+            Assert.AreEqual(1, b.topLeft.X);
+        }
+
+        [TestMethod]
+        public void MoveBoat_DirectionLEFT_Xdecrease_CheckResult()
+        {
+            Boat b = new Boat(2, new Tile(1, 1));
+            gm.MoveBoat(ref b, testMap8, Enum.Direction.LEFT);
+            Assert.AreEqual(1, b.topLeft.Y);
+            Assert.AreEqual(0, b.topLeft.X);
+        }
+
+        [TestMethod]
+        public void MoveBoat_DirectionRIGHT_Xincrease_CheckResult()
+        {
+            Boat b = new Boat(2, new Tile(1, 1));
+            gm.MoveBoat(ref b, testMap8, Enum.Direction.RIGHT);
+            Assert.AreEqual(1, b.topLeft.Y);
+            Assert.AreEqual(2, b.topLeft.X);
+        }
+        #endregion MoveBoat
+
+        #region GetNewTileWithDirection
+        [TestMethod]
+        public void GetNewTileWithDirection_NullTile_ThenNull()
+        {
+            Tile t = null;
+            Assert.IsNull(gm.GetNewTileWithDirection(t, Enum.Direction.TOP));
+        }
+
+        [TestMethod]
+        public void GetNewTileWithDirection_DirTOP_ThenYDecrease()
+        {
+            Tile tileToSend = new Tile(3, 3);
+            Assert.AreEqual(gm.GetNewTileWithDirection(tileToSend, Enum.Direction.TOP).Y, 2);
+        }
+
+        [TestMethod]
+        public void GetNewTileWithDirection_DirBOT_ThenYIncrease()
+        {
+            Tile tileToSend = new Tile(3, 3);
+            Assert.AreEqual(gm.GetNewTileWithDirection(tileToSend, Enum.Direction.BOTTOM).Y, 4);
+        }
+        [TestMethod]
+        public void GetNewTileWithDirection_DirLEFT_ThenXDecrease()
+        {
+            Tile tileToSend = new Tile(3, 3);
+            Assert.AreEqual(gm.GetNewTileWithDirection(tileToSend, Enum.Direction.LEFT).X, 2);
+        }
+
+        [TestMethod]
+        public void GetNewTileWithDirection_DirRIGTH_ThenXIncrease()
+        {
+            Tile tileToSend = new Tile(3, 3);
+            Assert.AreEqual(gm.GetNewTileWithDirection(tileToSend, Enum.Direction.RIGHT).X, 4);
+        }
+        #endregion GetNewTileWithDirection
     }
 }
--- BackEnd/GameManager.cs
+++ BackEnd/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackEnd.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,16 +16,121 @@ namespace BackEnd
         public bool IsMultiplayer { get; set; }
         public IA IA { get; set; }
 
+
+        /// <summary>
+        /// Generate a list of boat.
+        /// </summary>
+        /// <returns>Get a list of boat with length : 2,3,3,4,4,5.</returns>
+        #region + GenerateDefaultBoats() : List<Boat>
         public List<Boat> GenerateDefaultBoats()
         {
             List<Boat> boats = new List<Boat>();
-            boats.Add(new Boat(2));
-            boats.Add(new Boat(3));
-            boats.Add(new Boat(3));
-            boats.Add(new Boat(4));
-            boats.Add(new Boat(4));
-            boats.Add(new Boat(5));
+            boats.Add(new Boat(2, new Tile(1, 1)));
+            boats.Add(new Boat(3, new Tile(1, 1)));
+            boats.Add(new Boat(3, new Tile(1, 1)));
+            boats.Add(new Boat(4, new Tile(1, 1)));
+            boats.Add(new Boat(4, new Tile(1, 1)));
+            boats.Add(new Boat(5, new Tile(1, 1)));
             return boats;
+        }
+        #endregion + GenerateDefaultBoats() : List<Boat>
+
+        /// <summary>
+        /// Change boat position and renew boat's tiles used.
+        /// </summary>
+        /// <param name="boat">Boat to move.</param>
+        /// <param name="m">Map where boat is.</param>
+        /// <param name="dir">Direction of the movement.</param>
+        /// <returns>TRUE if boat is moved, else FALSE.</returns>
+        #region + MoveBoat(Boat, Map, Direction) : boolean
+        public bool MoveBoat(ref Boat boat, Map m, Direction dir)
+        {
+            Tile topleft = boat.topLeft;
+            Tile newTopLeft = GetNewTileWithDirection(topleft, dir);
+
+            #region Verifications
+            if (newTopLeft == null || !newTopLeft.IsOnMap(m))
+            {
+                return false;
+            }
+            #endregion Verifications
+
+            List<Tile> newTilesUsed = boat.GenerateTilesUsed(newTopLeft, boat.Length, boat.Alignement);
+
+            #region Verifications
+            foreach (Tile tile in newTilesUsed)
+            {
+                if (!(tile.IsOnMap(m)))
+                {
+                    return false;
+                }
+            }
+            #endregion Verifications
+
+            boat.topLeft = newTopLeft;
+            boat.TilesUsed = newTilesUsed;
+            return true;
+        }
+        #endregion + MoveBoat(Boat, Map, Direction) : boolean
+
+        /// <summary>
+        /// Get tile with new X & Y.
+        /// </summary>
+        /// <param name="tile">Tile to move.</param>
+        /// <param name="dir">Direction where to move.</param>
+        /// <returns>NULL if tile given is NOT OK.</returns>
+        #region + GetNewTileWithDirection(Tile, Direction) : Tile
+        public Tile GetNewTileWithDirection(Tile tile, Direction dir)
+        {
+            Tile newTile = null;
+
+            #region Verifications
+            if (!(tile == null))
+            {
+                newTile = tile;
+            }
+            else
+            {
+                return newTile;
+            }
+            #endregion Verifications
+
+            switch (dir)
+            {
+                case Direction.TOP:
+                    newTile.Y -= 1;
+                    break;
+                case Direction.RIGHT:
+                    newTile.X += 1;
+                    break;
+                case Direction.BOTTOM:
+                    newTile.Y += 1;
+                    break;
+                case Direction.LEFT:
+                    newTile.X -= 1;
+                    break;
+            }
+            return newTile;
+        }
+        #endregion + GetNewTileWithDirection(Tile, Direction) : Tile
+
+        public Map SetTileState(Map m,State stateWanted, List<Tile> tileToBeEmpty)
+        {
+            Map newMap = m;
+
+            foreach(Tile tileToEmpty in tileToBeEmpty)
+            {
+                foreach(Tile tileInMap in m.Tiles)
+                {
+                    if(tileInMap.X == tileToEmpty.X && tileInMap.Y == tileToEmpty.Y)
+                    {
+                        int index = m.Tiles.IndexOf(tileInMap);
+                        newMap.Tiles[index].State = stateWanted;
+                    }
+                }
+            }
+
+            return newMap;
         }
     }
 }

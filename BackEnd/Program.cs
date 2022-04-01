3bd1caaca573d5631f9dbf6e4c02fa3c8f8f2941
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-01 22:20:29 +0200

    Debug

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -9,8 +9,10 @@ namespace BackEnd
             Map m = new Map(8);
             GameManager gm = new GameManager();
             gm.GenerateDefaultBoats();
-            Boat b = new Boat(3);
-            b.GenerateTilesUsed(new Tile(3, 4), b.Length, Enum.Alignement.HORIZONTAL, m);
+            Boat b = new Boat(3, new Tile(1, 1));
+            b = new Boat(2, new Tile(1, 1));
+            gm.MoveBoat(ref b, m, Enum.Direction.TOP);
+            b.GenerateTilesUsed(new Tile(3, 4), b.Length, Enum.Alignement.VERTICAL);
             //Console.WriteLine("Hello World!");
             Console.ReadLine();
         }

18557e01734021a170754e61107eec3a55c3de0b
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-01 22:15:17 +0200

    Boat

--- BackEnd.Test/BoatTester.cs
+++ BackEnd.Test/BoatTester.cs
@@ -5,7 +5,7 @@ namespace BackEnd.Test
     [TestClass]
     public class BoatTester
     {
-        Boat b = new Boat(0);
+        Boat b = new Boat(0, new Tile(1,1));
         Map blankMap = new Map(0);
         Map okMap = new Map(8);
         int okLength = 2;
@@ -13,6 +13,7 @@ namespace BackEnd.Test
         Tile okTile = new Tile(1, 1);
         Tile failTile = new Tile(-5, 0);
 
+        #region IsOnMap
         [TestMethod]
         public void Boat_IsOnMap_WithBlankGrid_ThenFalse()
         {
@@ -36,43 +37,61 @@ namespace BackEnd.Test
         {
             Assert.IsTrue(b.IsOnMap(okMap, okTile, okLength, Enum.Alignement.HORIZONTAL));
         }
+        #endregion IsOnMap
 
+        #region GenerateTilesUsed
         [TestMethod]
         public void Boat_GenerateTilesUsed_WithNegativeOrZeroLength_ThenNull()
         {
-            Assert.IsNull(b.GenerateTilesUsed(okTile, -1, Enum.Alignement.HORIZONTAL, okMap));
+            Assert.IsNull(b.GenerateTilesUsed(okTile, -1, Enum.Alignement.HORIZONTAL));
         }
 
         [TestMethod]
         public void Boat_GenerateTilesUsed_WithNOKTopLeft_ThenNull()
         {
-            Assert.IsNull(b.GenerateTilesUsed(failTile, 2, Enum.Alignement.HORIZONTAL, okMap));
+            Assert.IsNull(b.GenerateTilesUsed(failTile, 2, Enum.Alignement.HORIZONTAL));
         }
 
         [TestMethod]
         public void Boat_GenerateTilesUsed_WithOKTopLeft3Length_ThenListCount3()
         {
-            Assert.AreEqual(3, b.GenerateTilesUsed(okTile, 3, Enum.Alignement.HORIZONTAL, okMap).Count);
+            Assert.AreEqual(3, b.GenerateTilesUsed(okTile, 3, Enum.Alignement.HORIZONTAL).Count);
         }
 
         [TestMethod]
-        public void Boat_GenerateTilesUsed_CheckResult()
+        public void Boat_GenerateTilesUsed_Horizontal_CheckResult()
         {
-            Assert.AreEqual(new Tile(3, 4).X, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL, okMap)[0].X);
-            Assert.AreEqual(new Tile(3, 4).Y, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL, okMap)[0].Y);
+            Assert.AreEqual(3, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL)[0].X);
+            Assert.AreEqual(4, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL)[0].Y);
 
-            Assert.AreEqual(new Tile(4, 4).X, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL, okMap)[0].X);
-            Assert.AreEqual(new Tile(4, 4).Y, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL, okMap)[0].Y);
+            Assert.AreEqual(4, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL)[1].X);
+            Assert.AreEqual(4, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL)[1].Y);
 
-            Assert.AreEqual(new Tile(5, 4).X, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL, okMap)[0].X);
-            Assert.AreEqual(new Tile(5, 4).Y, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL, okMap)[0].Y);
+            Assert.AreEqual(5, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL)[2].X);
+            Assert.AreEqual(4, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.HORIZONTAL)[2].Y);
         }
 
+        [TestMethod]
+        public void Boat_GenerateTilesUsed_Vertical_CheckResult()
+        {
+            Assert.AreEqual(3, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.VERTICAL)[0].X);
+            Assert.AreEqual(4, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.VERTICAL)[0].Y);
+
+            Assert.AreEqual(3, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.VERTICAL)[1].X);
+            Assert.AreEqual(5, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.VERTICAL)[1].Y);
+
+            Assert.AreEqual(3, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.VERTICAL)[2].X);
+            Assert.AreEqual(6, b.GenerateTilesUsed(new Tile(3, 4), 3, Enum.Alignement.VERTICAL)[2].Y);
+        }
+        #endregion GenerateTilesUsed
+
+        #region AffectName
         [TestMethod]
         public void Boat_AffectName_CheckResult()
         {
             b.Length = 2;
             Assert.AreEqual("long2", b.AffectName());
         }
+        #endregion AffectName
     }
 }
--- BackEnd/Boat.cs
+++ BackEnd/Boat.cs
@@ -9,19 +9,48 @@ namespace BackEnd
         public int Length { get; set; }
         public Alignement Alignement { get; set; }
         public List<Tile> TilesUsed { get; set; }
+        public Tile topLeft { get; set; }
         public string Name { get; set; }
 
-        public Boat(int length)
+
+
+
+
+        #region Constructors
+        public Boat(int length, Tile topleft)
         {
             Length = length;
+            topLeft = topleft;
+            Alignement = Alignement.HORIZONTAL;
             Name = AffectName();
+            TilesUsed = GenerateTilesUsed(topleft, length,Alignement);
         }
+        #endregion Constructors
 
+
+
+
+
+        /// <summary>
+        /// Applied default name for a boat : long<Length>.
+        /// </summary>
+        /// <returns>String with name.</returns>
+        #region + AffectName() : string
         public string AffectName()
         {
             return new string("long"+Length);
         }
+        #endregion + AffectName() : string
 
+        /// <summary>
+        /// Check if the boat placement is ok or not.
+        /// </summary>
+        /// <param name="m">Map to place the boat.</param>
+        /// <param name="topLeft">TopLeft tile of the boat.</param>
+        /// <param name="length">Length of the boat.</param>
+        /// <param name="alignement">Alignement of the boat.</param>
+        /// <returns>TRUE if placement OK, else returns FALSE.</returns>
+        #region + IsOnMap(Map, Tile, int, Alignement) : boolean
         public bool IsOnMap(Map m, Tile topLeft, int length, Alignement alignement)
         {
             if (m.Size < 1) { return false; }
@@ -36,16 +65,29 @@ namespace BackEnd
                 topLeft.X += length;
             }
             if(!(topLeft.IsOnMap(m))){ return false; }
+            this.topLeft = topLeft;
             return true;
         }
+        #endregion + IsOnMap(Map, Tile, int, Alignement) : boolean
+        #region + IsOnMap(Map) : boolean
+        public bool IsOnMap(Map m)
+        {
+            return this.IsOnMap(m, this.topLeft, this.Length, this.Alignement);
+        }
+        #endregion + IsOnMap(Map) : boolean
 
-        public List<Tile> GenerateTilesUsed(Tile topLeft, int length, Alignement alignement, Map m)
+
+        /// <summary>
+        /// Get a list of tile used by the boat.
+        /// </summary>
+        /// <param name="topLeft">Top left tile of the boat.</param>
+        /// <param name="length">Length of the boat.</param>
+        /// <param name="alignement">Alignement of the boat.</param>
+        /// <returns>Returns a LIST of TILE. If the boat is not Ok, the list is NULL.</returns>
+        #region + GenerateTilesUsed(Tile, int, Alignement) : List<Tile>
+        public List<Tile> GenerateTilesUsed(Tile topLeft, int length, Alignement alignement)
         {
             List<Tile> tilesUsed = null;
-            if (!(topLeft.IsOnMap(m)))
-            {
-                return tilesUsed;
-            }
             if(length < 1)
             {
                 return tilesUsed;
@@ -56,16 +98,29 @@ namespace BackEnd
             {
                 if(alignement == Alignement.VERTICAL)
                 {
-                    Tile t = new Tile(topLeft.X, topLeft.Y+(i+1));
+                    Tile t = new Tile(topLeft.X, topLeft.Y + i);
+                    t.State = State.IsBoat;
+                    Console.WriteLine("VERTICAL : "+t.X+" "+t.Y);
                     tilesUsed.Add(t);
                 }
                 else
                 {
-                    Tile t = new Tile(topLeft.X + (i + 1), topLeft.Y);
+                    Tile t = new Tile(topLeft.X + i, topLeft.Y);
+                    t.State = State.IsBoat;
+                    Console.WriteLine("HORIZONTHAL : "+t.X + " " + t.Y);
                     tilesUsed.Add(t);
                 }
             }
+            this.topLeft = topLeft;
             return tilesUsed;
         }
+        #endregion + GenerateTilesUsed(Tile, int, Alignement, Map) : List<Tile>
+        #region + GenerateTilesUsed(Map)
+        public List<Tile> GenerateTilesUsed()
+        {
+            return GenerateTilesUsed(topLeft, Length, Alignement);
+        }
+        #endregion + GenerateTilesUsed(Map)
+
     }
 }

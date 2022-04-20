8d6b8651a6b5c78fd7a138247165d745777d0370
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-20 10:31:51 +0200

    TEST : Tile - verifyX

--- Battleship.Test/TileTester.cs
+++ Battleship.Test/TileTester.cs
@@ -1,9 +1,43 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
 
 namespace Battleship.Test
 {
     [TestClass]
     public class TileTester
     {
+        
+        #region verifyX
+        [TestMethod]
+        public void verifyX_XNegative_ThenFalse()
+        {
+            Tile tile = new Tile() { X = -10};
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyX", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = {};
+            Assert.IsFalse((Boolean)methodInfo.Invoke(tile, parameters));
+        }
+
+        [TestMethod]
+        public void verifyX_XZero_ThenTrue()
+        {
+            Tile tile = new Tile() { X = 0 };
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyX", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsFalse((Boolean)methodInfo.Invoke(tile, parameters));
+        }
+
+        [TestMethod]
+        public void verifyX_XPositive_ThenTrue()
+        {
+            Tile tile = new Tile() { X = 10 };
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyX", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsFalse((Boolean)methodInfo.Invoke(tile, parameters));
+        }
+        #endregion verifyX
     }
 }

dfec7fc7883f2ad3173b80b53ad176cba1498f23
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-20 10:57:26 +0200

    TEST : Boat - verifyLength

--- Battleship.Test/BoatTester.cs
+++ Battleship.Test/BoatTester.cs
@@ -70,5 +70,47 @@ namespace Battleship.Test
             Assert.IsTrue((Boolean)methodInfo.Invoke(b, parameters));
         }
         #endregion verifyName
+
+        #region verifyLength
+        [TestMethod]
+        public void verifyLength_LengthNegative_ThenFalse()
+        {
+            Boat b = new Boat() { Length = -10 };
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyLength", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsFalse((Boolean)methodInfo.Invoke(b, parameters));
+        }
+
+        [TestMethod]
+        public void verifyLength_LengthZero_ThenFalse()
+        {
+            Boat b = new Boat() { Length = 0 };
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyLength", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsFalse((Boolean)methodInfo.Invoke(b, parameters));
+        }
+
+        [TestMethod]
+        public void verifyLength_LengthPositive_ThenTrue()
+        {
+            Boat b = new Boat() { Length = 1 };
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyLength", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsTrue((Boolean)methodInfo.Invoke(b, parameters));
+        }
+
+        [TestMethod]
+        public void verifyLength_LengthMoreThan5_ThenFalse()
+        {
+            Boat b = new Boat() { Length = 6};
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyLength", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsFalse((Boolean)methodInfo.Invoke(b, parameters));
+        }
+        #endregion verifyLength
     }
 }

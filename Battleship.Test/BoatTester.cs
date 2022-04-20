4992e864911d1ed1c0a7938c1de8bb99cd99418b
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-20 10:50:11 +0200

    TEST : Boat - verifyId

--- Battleship.Test/BoatTester.cs
+++ Battleship.Test/BoatTester.cs
@@ -1,10 +1,42 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System;
+using System.Reflection;
 
 namespace Battleship.Test
 {
     [TestClass]
     public class BoatTester
     {
+        #region verifyId
+        [TestMethod]
+        public void verifyId_IdNegative_ThenFalse()
+        {
+            Boat b = new Boat() { Id = -10};
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyId", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsFalse((Boolean)methodInfo.Invoke(b, parameters));
+        }
+
+        [TestMethod]
+        public void verifyId_IdZero_ThenFalse()
+        {
+            Boat b = new Boat() { Id = 0};
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyId", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsFalse((Boolean)methodInfo.Invoke(b, parameters));
+        }
+
+        [TestMethod]
+        public void verifyId_IdPositive_ThenFalse()
+        {
+            Boat b = new Boat() { Id = 4};
+
+            MethodInfo methodInfo = typeof(Tile).GetMethod("verifyId", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { };
+            Assert.IsTrue((Boolean)methodInfo.Invoke(b, parameters));
+        }
+        #endregion verifyId
     }
 }

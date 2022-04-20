6dcb40801a95ef11c6b9391039e359ec05a3e206
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-20 10:18:39 +0200

    Création classe Player

---
+++ Battleship/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Map Map { get; set; }
+        public List<Boat> Boats { get; set; }
+
+        public Player()
+        {
+            Boats = new List<Boat>();
+        }
+
+        // Factory public Player(int,int){}
+
+        public Boolean Verify()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Boolean verifyId()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Boolean verifyName()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Boolean verifyMap()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Boolean verifyBoats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

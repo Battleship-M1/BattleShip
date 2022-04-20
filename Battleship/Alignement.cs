e0b20600d9b3d306c4aaf70793c3b62c5deae7dc
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-20 11:35:35 +0200

    Création enum Alignement

---
+++ Battleship/Alignement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public enum Alignement
+    {
+        HORIZONTHAL,
+        VERTICAL,
+        DIAG_BOTTOM,
+        DIAG_TOP
+    }
+}

28825750125ff72ac458849c976be17ec1d49545
AUBO <augustin.bourgis.auditeur@lecnam.net>
2022-04-20 10:18:54 +0200

    Création classe Tile

---
+++ Battleship/Tile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class Tile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public TileState State { get; set; }
+        public ShotState ShotState { get; set; }
+
+        public Tile() { }
+        // Factory public Tile(int,int){}
+
+        public Boolean Verify()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Boolean verifyX()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Boolean verifyY()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
